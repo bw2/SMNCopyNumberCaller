1b14fce475dc9f513f524bebcd7aef939df09562
bw2 <ben.weisburd@gmail.com>
2024-10-06 06:21:43 -0400

    fixing data import

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
 		"smn_caller.depth_calling",
 		"smn_caller.data",
     ],
-    package_dir={"": ""},
     include_package_data=True,
     python_requires=">=3.9",
     license="PolyForm Strict License 1.0.0",
